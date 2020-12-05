5243e3b19248be72e942be1de8b026301669d8b3
olb <2453684+olb17@users.noreply.github.com>
2020-12-05 09:53:30 +0100

    Delete secret.exs

--- config/secret.exs
+++
@@ -1,5 +0,0 @@
-use Mix.Config
-
-config :advent_of_code, AdventOfCode.Input,
-  session_cookie:
-    "session=53616c7465645f5f4b3bcf8d0521d47c0b66134b8279380059cd7fde1794b57a41762ecd66010d4b12c5d60a1f515b6c"
