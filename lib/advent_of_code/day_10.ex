9690c128597c2ee5e26bc3aa85cb912f9e003c02
olb <2453684+olb17@users.noreply.github.com>
2020-12-11 12:49:43 +0000

    Day 10

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -1,7 +1,72 @@
 defmodule AdventOfCode.Day10 do
   def part1(args) do
+    outlets =
+      args
+      |> parse_args()
+      |> Enum.sort()
+      |> Enum.reverse()
+
+    from_outlet = outlets ++ [0]
+    from_device = [hd(outlets) + 3] ++ outlets
+
+    result =
+      Enum.zip(from_outlet, from_device)
+      |> Enum.map(&(elem(&1, 1) - elem(&1, 0)))
+      |> Enum.group_by(& &1)
+      |> Enum.map(fn {key, val} -> {key, Enum.count(val)} end)
+      |> Map.new()
+
+    result[3] * result[1]
   end
 
   def part2(args) do
+    outlets =
+      args
+      |> parse_args()
+      |> Enum.sort()
+      |> Enum.reverse()
+
+    from_outlet = outlets ++ [0]
+    from_device = [hd(outlets) + 3] ++ outlets
+
+    {acc, last} =
+      Enum.zip(from_outlet, from_device)
+      |> Enum.reduce({1, 0}, fn {left, right}, {acc, nb} ->
+        gap = right - left
+
+        cond do
+          gap == 3 && nb > 0 ->
+            {acc * nb_combin(nb), 0}
+
+          gap == 3 && nb == 0 ->
+            {acc, nb}
+
+          gap == 1 ->
+            {acc, nb + 1}
+        end
+      end)
+
+    if last != 0 do
+      acc * nb_combin(last)
+    else
+      acc
+    end
+  end
+
+  def nb_combin(nb) when nb > 2 do
+    :math.pow(2, nb - 1) - :math.pow(2, nb - 3) + 1
+  end
+
+  def nb_combin(nb) when nb == 2, do: 2
+
+  def nb_combin(nb) when nb == 1, do: 1
+
+  def parse_args(args) do
+    args
+    |> String.trim()
+    |> String.split(["\n"])
+    |> Enum.map(fn elt ->
+      Integer.parse(elt) |> elem(0)
+    end)
   end
 end
--- test/advent_of_code/day_10_test.exs
+++ test/advent_of_code/day_10_test.exs
@@ -3,17 +3,36 @@ defmodule AdventOfCode.Day10Test do
 
   import AdventOfCode.Day10
 
+  @args """
+    16
+  10
+  15
+  5
+  1
+  11
+  7
+  19
+  6
+  12
+  4
+  """
   @tag :skip
   test "part1" do
-    input = nil
+    input = @args
     result = part1(input)
 
-    assert result
+    assert result == 35
   end
 
-  @tag :skip
+  @tag :skipno
   test "part2" do
-    input = nil
+    assert nb_combin(1) == 1
+    assert nb_combin(2) == 2
+    assert nb_combin(3) == 4
+    assert nb_combin(4) == 7
+    assert nb_combin(5) == :math.pow(2, 4) - 3
+
+    input = @args
     result = part2(input)
 
     assert result
